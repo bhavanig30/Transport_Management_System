7d9ee86935f00b6995ffc024a3c8575e10672fdc
Kavi1mg <kavibharathimg@gmail.com>
2025-02-20 16:17:47 +0530

    backend

--- tms/src/App.js
+++ tms/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Frontend/Pages/Login";
-import FCForm from "./Frontend/Pages/FC";
-import TransactionDetails from "./Frontend/Pages/Transaction Details";
-import VehiclePermitForm from "./Frontend/Pages/VehiclePermitForm";
-import DriverAllotmentForm from "./Frontend/Pages/DriverAllotment";
-import TravellerAllotment from "./Frontend/Pages/TravellerAllotment";
-import StageMasterForm from "./Frontend/Pages/StageMaster";
-import RouteMasterForm from "./Frontend/Pages/RouteMaster";
-import InsuranceForm from "./Frontend/Pages/InsuranceDetails";
-function App() {
+import Home from "./Frontend/Pages/home";
+
+
+const App = () => {
   return (
-    <div>
-      <DriverAllotmentForm />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
--- tms/src/Frontend/Pages/Login.js
+++ tms/src/Frontend/Pages/Login.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:5000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        alert("Login Successful!");
+        window.location.href = "/home"; // Redirect to home page
+      } else {
+        alert("Invalid Username or Password!");
+      }
+    } catch (error) {
+      console.error("Login Error:", error);
+    }
+  };
+
   return (
     <div className="login-container">
-      {/* Header Section */}
       <div className="header">
         <img src="/Images/Logo.png" alt="Left Logo" className="logo left-logo" />
         <div className="header-content">
@@ -20,26 +45,34 @@ const Login = () => {
         <img src="/Images/Founder.jpg" alt="Right Logo" className="logo right-logo" />
       </div>
 
-      {/* Login Box */}
       <div className="login">
         <div className="login-box">
           <h1>Login</h1>
+          <form onSubmit={handleLogin}>
+            <label>User Name</label>
+            <input
+              type="text"
+              placeholder="Enter User Name"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-          <label>User Name</label>
-          <input type="text" placeholder="Enter User Name" />
-          
-          <label>Password</label>
-          <input type="password" placeholder="Enter Password" />
+            <div className="stay-signed-in">
+              <input type="checkbox" id="staySignedIn" />
+              <label htmlFor="staySignedIn">Stay signed in</label>
+            </div>
 
-          {/* Stay Signed In Checkbox */}
-          <div className="stay-signed-in">
-            <input type="checkbox" id="staySignedIn" />
-            <label htmlFor="staySignedIn">Stay signed in</label>
-          </div>
+            <button type="submit" className="login-button">Login</button>
+          </form>
 
-          <button className="login-button">Login</button>
-
-          {/* Forgot Password Link */}
           <div className="forgot-password">
             <a href="#">Forgot password / username</a>
           </div>
