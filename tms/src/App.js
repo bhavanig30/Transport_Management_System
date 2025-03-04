858dc8666ebe52bb41fe006b113bf2147ab85d06
Narmada-94 <narmadam263@gmail.com>
2025-03-04 16:32:52 +0530

    Saved local changes

--- tms/src/App.js
+++ tms/src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Frontend/Pages/Login";
 import Home from "./Frontend/Pages/Home";
-import VehicleMaster from "./Frontend/Pages/Vehicle Master/VehicleMaster";
+import VehicleMaster from   "./Frontend/Pages/Vehicle Master/VehicleMaster";
 import VehiclePermitForm from "./Frontend/Pages/VehiclePermitForm";
 import VehicleMasterView from "./Frontend/Pages/Vehicle Master/VehicleMasterView";
 import VehicleMasterAdd from "./Frontend/Pages/Vehicle Master/VehicleMasterAdd";
-
+import Add from "./Frontend/Pages/Insurance/InsuranceAdd";
+import FCForm from "./Frontend/Pages/FC";
+import StageMasterForm from "./Frontend/Pages/StageMaster";
 
 const App = () => {
   return (
-    <VehicleMasterAdd />
+    <VehicleMasterAdd/>
   );
 };
 
