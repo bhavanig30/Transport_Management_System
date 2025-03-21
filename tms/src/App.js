f3af349bb8cfb46a6323434b480abf806b8dd29d
Narmada-94 <narmadam263@gmail.com>
2025-03-21 10:10:36 +0530

    App

--- tms/src/App.js
+++ tms/src/App.js
@@ -7,6 +7,11 @@ import AddStage from "./Frontend/pages/StageMaster/AddStage";
 import AddRoute from "./Frontend/pages/RouteMaster/AddRoute";
 import AddDriver from "./Frontend/pages/DriverAllotment/AddDriver";
 import AddTraveller from "./Frontend/pages/TravellerAllotment/AddTraveller";
+import ViewVehicle from "./Frontend/pages/VehicleMaster/ViewVehicle";
+import AddVehicle from "./Frontend/pages/VehicleMaster/AddVehicle";
+import AddPermit from "./Frontend/pages/VehicleMaster/PermitDetails/AddPermit";
+import AddInsurance from "./Frontend/pages/VehicleMaster/InsuranceDetails/AddInsurance";
+import AddFC from "./Frontend/pages/VehicleMaster/FCDetails/AddFC";
 
 
 const App = () => {
@@ -21,6 +26,13 @@ const App = () => {
           <Route path="adddriver" element={<AddDriver />} />
           <Route path="addtraveller" element={<AddTraveller />} />
         </Route>
+        
+        <Route path="/home/vehiclemaster/viewvehicle" element={<ViewVehicle />} />
+        <Route path="/home/vehiclemaster/addvehicle" element={<AddVehicle />}/>
+        <Route path="/home/vehiclemaster/addpermit" element={<AddPermit />} />
+        <Route path="/home/vehiclemaster/addinsurance" element={<AddInsurance/>} />
+        <Route path="/home/vehiclemaster/addfc" element={<AddFC  />} />
+
       </Routes>
     </Router>
   );
